933f42683b00f242258d7c2d889bbb7a35a1c667
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-08-28 13:10:46 -0700

    Finished Creating Helper Function

--- checklist.py
+++ checklist.py
@@ -1,7 +1,8 @@
 
-
+# Creates our Checklist
 checklist = []
 
+# Define Functions
 def create(item):
     checklist.append(item)
 
@@ -24,7 +25,32 @@ def mark_completed(index):
     checklist[index] = "{}{}".format("√", checklist[index])
     # update(index, "{}{}".format("√", checklist[index])
 
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
 
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_value = user_input("Please Enter a value:")
+    print(user_value)
 
 def test():
     create("purple sox")
@@ -40,4 +66,5 @@ def test():
 
     list_all_items()
 
+# Run Tests
 test()
